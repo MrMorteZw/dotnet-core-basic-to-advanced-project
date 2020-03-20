cf85b588c883b5fd0fe19fb2182bcd9b820d72b2
DASH MORTEZW <43701982+MrMorteZw@users.noreply.github.com>
2020-03-20 05:12:09 +0330

    Add Main File

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LEARN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldshu");
+        }
+    }
+}
